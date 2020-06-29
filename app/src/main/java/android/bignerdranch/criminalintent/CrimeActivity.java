9fbdb317366f82ac74530b2d6382867059be8fc4
AlekseevArtem <tema9991@mail.ru>
2020-06-30 01:01:38 +0300

    Chapter 8

---
+++ app/src/main/java/android/bignerdranch/criminalintent/CrimeActivity.java
@@ -0,0 +1,11 @@
+package android.bignerdranch.criminalintent;
+
+import androidx.fragment.app.Fragment;
+
+public class CrimeActivity extends SingleFragmentActivity {
+
+    @Override
+    protected Fragment createFragment() {
+        return new CrimeFragment();
+    }
+}
